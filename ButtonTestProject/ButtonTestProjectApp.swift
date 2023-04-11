09b3b83db0b909a7874b202eae4eb7b7e24dd54f
Chris Clampitt <christian.clampitt@nyu.edu>
2023-04-11 12:10:21 -0700

    Initial Commit

---
+++ ButtonTestProject/ButtonTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTestProjectApp.swift
+//  ButtonTestProject
+//
+//  Created by Christian J Clampitt on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
